1823217164bd7bb533626785fbe24b9598d960a4
Adonis Mendoza <adonis.mendoza@outlook.com>
2018-10-18 12:33:56 -0400

    Basic Mathematical Operations

---
+++ Algorithm/BasicMathematicalOperations.py
@@ -0,0 +1,8 @@
+#Basic Mathematical Operations
+
+# Your task is to create a function that does four basic mathematical operations.
+# The function should take three arguments - operation(string/char), value1(number), value2(number).
+# The function should return result of numbers after applying the chosen operation.
+
+def basic_op(operator, value1, value2):
+    return eval('{} {} {}'.format(str(value1),str(operator),str(value2)))
