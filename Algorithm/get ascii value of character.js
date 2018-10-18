c0e6a213ea2bd11aa5b38e28d3a12524154ced03
Adonis Mendoza <adonis.mendoza@outlook.com>
2018-10-18 16:30:23 -0400

    get ascii value of character

---
+++ Algorithm/get ascii value of character.js
@@ -0,0 +1,7 @@
+// get ascii value of character
+
+// write a function getASCII which inputs a character 
+// and returns the corresponding ascii value for that 
+// character. Example : getASCII('A') => 65
+
+let getASCII = (c)=> c.charCodeAt(0);
