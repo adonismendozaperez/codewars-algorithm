62c3aa957dffd54656c39d054ca040caa69a5925
Adonis Mendoza <adonis.mendoza@outlook.com>
2018-10-18 12:30:42 -0400

    Is he gonna survive

---
+++ Algorithm/IsHeGonnaSurvive.py
@@ -0,0 +1,13 @@
+#Is he gonna survive?
+#  A hero is on his way to the castle to complete his mission.
+#  However, he's been told that the castle is surrounded with 
+#  a couple of powerful dragons! each dragon takes 2 bullets to 
+#  be defeated, our hero has no idea how many bullets he should carry..
+#  Assuming he's gonna grab a specific given number of bullets and move
+#  forward to fight another specific given number of dragons, will he survive?
+def hero(bullets, dragons):
+    total_bullets = dragons * 2
+    if bullets >= total_bullets:
+        return True
+    else:
+        return False
